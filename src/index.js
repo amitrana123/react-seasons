f4b558903736de650c06aab187edd99ca5519d79
Amit Rana <amit.rana@agilitypr.com>
2022-06-29 05:46:48 +0530

    Seasons initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+import './style/App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position),
+//         (err) => console.log(err)
+//     );
+//     return <div>Hi There</div>
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { lat: null, errorMessage: '' };        
+    // }
+
+    state = { lat: null, errorMessage: '' };
+
+   componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat: position.coords.latitude})
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
+   }
+
+   renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            //return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat = {this.state.lat}/>
+        }
+
+        return <Spinner message="Please accept location request" />;
+   }
+
+    render() {     
+        
+        return (
+            <div className='border red'>
+                {this.renderContent()}
+            </div>
+        )
+        
+        // return (
+        //     <div>
+        //         Latitude: {this.state.lat}
+        //         <br />
+        //         Error: {this.state.errorMessage}
+        //     </div>
+
+        //     )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
